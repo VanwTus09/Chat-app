d61855612796a8d2e7ad893e996c411edb1cd9f7
VanwTus09 <tunguyen/05112004@gmail.com>
2025-03-10 01:18:46 +0700

    day1

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("cookie-session");
+const passport = require("passport");
+require("dotenv").config();
+require("./config/passport");
+
+const authRoutes = require("./routes/Authroutes")
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use("/auth", authRoutes);
+
+// Kết nối MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
